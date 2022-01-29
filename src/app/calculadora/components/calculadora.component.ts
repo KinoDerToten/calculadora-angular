6df7a767ef93e9daee00a30b798775a804e5bfa1
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2022-01-28 21:56:50 -0400

    criando a operacao de calculo

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -7,16 +7,60 @@ import { CalculadoraService } from '../services';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
-  public display: string = this.calculadoraService.display;
+  public display: string = '0';
+  public primeiroValor: string;
+  public operacao: string = null;
+  public segundoValor: string;
+  public resultado: number;
 
-  constructor(private calculadoraService: CalculadoraService) { }
+  constructor(public calculadoraService: CalculadoraService) {
+
+  }
 
   ngOnInit(): void {
   }
 
-  adicionaNumero(valor: string): string {
-    this.display = this.calculadoraService.adicionarNumero(valor);
-    return this.display;
+  adicionarNumero(numero: string): void {
+    if (this.display === '0') {
+      this.display = '';
+      this.display += numero;
+    } else {
+      this.display += numero;
+    }
   }
 
+  operacaoCalculo(operacao: string): void {
+    if (this.operacao == null) {
+      this.primeiroValor = this.display;
+      this.display = '';
+      this.operacao = operacao;
+    }
+  }
+
+  calcular(): string {
+    if (this.operacao != null) {
+      this.segundoValor = this.display;
+    }
+
+    switch (this.operacao) {
+      case '+':
+        this.resultado = parseFloat(this.primeiroValor) + parseFloat(this.segundoValor);
+        break;
+      case '-':
+        this.resultado = parseFloat(this.primeiroValor) - parseFloat(this.segundoValor);
+        break;
+      case '/':
+        this.resultado = parseFloat(this.primeiroValor) / parseFloat(this.segundoValor);
+        break;
+      case '*':
+        this.resultado = parseFloat(this.primeiroValor) * parseFloat(this.segundoValor);
+        break;
+      default:
+        this.resultado = 0;
+        break;
+    }
+
+    this.display = this.resultado.toString();
+    return this.display;
+  }
 }
--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -4,19 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculadoraService {
-  public display: string = '0';
 
   constructor() { }
 
-
-
-  adicionarNumero(valor: string): string {
-    if (this.display === '0') {
-      this.display = '';
-      this.display += valor;
-    } else {
-      this.display += valor;
-    }
-    return this.display;
-  }
 }
