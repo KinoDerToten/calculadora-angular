8ddd79c868b5eca8263be7fd29ce31d9c07d52bd
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2022-02-16 22:11:11 -0400

    calculadora finalizada

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -1,4 +1,3 @@
-import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { CalculadoraService } from '../services';
 
@@ -8,14 +7,9 @@ import { CalculadoraService } from '../services';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
-  public display: string = '0';
-  public valorAtual: string = ' ';
-  public operacao: string = null;
-  public novoValor: string = ' ';
-  public resultado: number = null;
-  public valorPendente: number = null;
+  public display: string = this.calculadoraService.display;
 
-  constructor(public calculadoraService: CalculadoraService) {
+  constructor(private calculadoraService: CalculadoraService) {
 
   }
 
@@ -23,121 +17,34 @@ export class CalculadoraComponent implements OnInit {
   }
 
   adicionarNumero(numero: string): void {
-    if (this.operacao === null) {
-      this.valorAtual += numero;
-      this.display = ''
-      this.numeroDecimal();
-      this.display = this.valorAtual;
-    } else {
-      this.display = '';
-      this.novoValor += numero;
-      this.numeroDecimalNovoValor();
-      this.display = this.novoValor;
-    }
+    this.display = this.calculadoraService.adicionarNumero(numero);
   }
 
   numeroDecimal(): void {
-    if (this.valorAtual.indexOf('.') > -1) {
-      let [parteInteira, parteDecimal]: string[] = this.valorAtual.split('.');
-      if (parteInteira === ' ') {
-        parteInteira = '0';
-      }
-      this.valorAtual = [parteInteira, parteDecimal].toString();
-      this.valorAtual = this.valorAtual.replace(",", ".");
-      console.log([parteInteira]);
-    }
+    this.calculadoraService.numeroDecimal();
   }
 
   numeroDecimalNovoValor(): void {
-    if (this.novoValor.indexOf('.') > -1) {
-      let [parteInteira, parteDecimal]: string[] = this.novoValor.split('.');
-      this.novoValor = [parteInteira, parteDecimal].toString();
-      this.novoValor = this.novoValor.replace(",", ".");
-    }
+    this.calculadoraService.numeroDecimalNovoValor();
   }
 
   operacaoCalculo(operacao: string): void {
-    if (this.operacao != null && this.novoValor != ' ') {
-      this.calcular();
-    }
-    this.operacao = operacao;
+    this.display = this.calculadoraService.operacaoCalculo(operacao);
   }
 
   calcular(): void {
-    switch (this.operacao) {
-      case '+':
-        this.resultado = parseFloat(this.valorAtual) + parseFloat(this.novoValor);
-        console.log(this.valorAtual + ' ' + this.novoValor);
-        break;
-      case '-':
-        this.resultado = parseFloat(this.valorAtual) - parseFloat(this.novoValor);
-        console.log(this.valorAtual + ' ' + this.novoValor);
-        break;
-      case '/':
-        this.resultado = parseFloat(this.valorAtual) / parseFloat(this.novoValor);
-        console.log(this.valorAtual + ' ' + this.novoValor);
-        break;
-      case '*':
-        this.resultado = parseFloat(this.valorAtual) * parseFloat(this.novoValor);
-        console.log(this.valorAtual + ' ' + this.novoValor);
-        break;
-      default:
-        this.resultado = 0;
-        break;
-    }
-    this.display = this.resultado.toString();
-    this.valorAtual = this.resultado.toString();
-    this.novoValor = ' ';
+    this.display = this.calculadoraService.calcular();
   }
 
   inverterOperacao(): void {
-    if (this.display === '0') {
-      this.valorPendente = parseFloat(this.display) * -1
-      this.display = this.valorPendente.toString();
-    } else if (this.operacao === null && parseFloat(this.valorAtual) >= 0) {
-      this.valorPendente = parseFloat(this.valorAtual) * -1;
-      this.valorAtual = this.valorPendente.toString();
-      this.display = this.valorAtual;
-    } else {
-      this.valorPendente = parseFloat(this.valorAtual) * -1;
-      this.valorAtual = this.valorPendente.toString();
-      this.display = this.valorAtual;
-    }
+    this.calculadoraService.inverterOperacao();
   }
 
   limpar(): void {
-    this.display = '0';
-    this.valorAtual = ' ';
-    this.operacao = null;
-    this.novoValor = ' ';
-    this.resultado = null;
-    this.valorPendente = null;
+    this.calculadoraService.limpar();
   }
 
   percent(): void {
-    let valuePercent: number;
-    if (this.novoValor != ' ') {
-      switch (this.operacao) {
-        case '+':
-          valuePercent = (parseFloat(this.novoValor) / 100) * parseFloat(this.valorAtual);
-          this.novoValor = valuePercent.toString();
-          break;
-        case '-':
-          valuePercent = (parseFloat(this.novoValor) / 100) * parseFloat(this.valorAtual);
-          this.novoValor = valuePercent.toString();
-          break;
-        case '*':
-          valuePercent = (parseFloat(this.novoValor) / 100);
-          this.novoValor = valuePercent.toString();
-          break;
-        case '/':
-          valuePercent = (parseFloat(this.novoValor) / 100);
-          this.novoValor = valuePercent.toString();
-          break;
-        default:
-          'selecione uma operação correta'
-          break;
-      }
-    }
+    this.calculadoraService.percent();
   }
 }
--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -4,7 +4,139 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculadoraService {
+  public display = '0';
+  public valorAtual: string = ' ';
+  public operacao: string = null;
+  public novoValor: string = ' ';
+  public resultado: number = null;
+  public valorPendente: number = null;
 
   constructor() { }
+
+  adicionarNumero(numero: string): string {
+    if (this.operacao === null) {
+      this.valorAtual += numero;
+      this.display = ''
+      this.numeroDecimal();
+      this.display = this.valorAtual;
+    } else {
+      this.display = '';
+      this.novoValor += numero;
+      this.numeroDecimalNovoValor();
+      this.display = this.novoValor;
+    }
+    return this.display;
+  }
+
+  numeroDecimal(): void {
+    if (this.valorAtual.indexOf('.') > -1) {
+      let [parteInteira, parteDecimal]: string[] = this.valorAtual.split('.');
+      if (parteInteira === ' ') {
+        parteInteira = '0';
+      }
+      this.valorAtual = [parteInteira, parteDecimal].toString();
+      this.valorAtual = this.valorAtual.replace(",", ".");
+    }
+  }
+
+  numeroDecimalNovoValor(): void {
+    if (this.novoValor.indexOf('.') > -1) {
+      let [parteInteira, parteDecimal]: string[] = this.novoValor.split('.');
+      this.novoValor = [parteInteira, parteDecimal].toString();
+      this.novoValor = this.novoValor.replace(",", ".");
+    }
+  }
+
+  operacaoCalculo(operacao: string): string {
+    let resultado: string;
+    if (this.operacao != null && this.novoValor != ' ') {
+      resultado = this.calcular();
+      this.operacao = operacao;
+      return resultado
+    } else {
+      this.operacao = operacao;
+      return this.valorAtual
+    }
+  }
+
+  calcular(): string {
+    switch (this.operacao) {
+      case '+':
+        this.resultado = parseFloat(this.valorAtual) + parseFloat(this.novoValor);
+        break;
+      case '-':
+        this.resultado = parseFloat(this.valorAtual) - parseFloat(this.novoValor);
+        break;
+      case '/':
+        this.resultado = parseFloat(this.valorAtual) / parseFloat(this.novoValor);
+        break;
+      case '*':
+        this.resultado = parseFloat(this.valorAtual) * parseFloat(this.novoValor);
+        break;
+      default:
+        'selecione uma operacao'
+        break;
+    }
+    this.display = this.resultado.toString();
+    this.valorAtual = this.resultado.toString();
+    this.novoValor = ' ';
+
+    return this.display
+  }
+
+  inverterOperacao(): string {
+    if (this.display === '0') {
+      this.valorPendente = parseFloat(this.display) * -1
+      this.display = this.valorPendente.toString();
+    } else if (this.operacao === null && parseFloat(this.valorAtual) >= 0) {
+      this.valorPendente = parseFloat(this.valorAtual) * -1;
+      this.valorAtual = this.valorPendente.toString();
+      this.display = this.valorAtual;
+    } else {
+      this.valorPendente = parseFloat(this.valorAtual) * -1;
+      this.valorAtual = this.valorPendente.toString();
+      this.display = this.valorAtual;
+    }
+
+    return this.display
+  }
+
+  limpar(): string {
+    this.display = '0';
+    this.valorAtual = ' ';
+    this.operacao = null;
+    this.novoValor = ' ';
+    this.resultado = null;
+    this.valorPendente = null;
+
+    return this.display
+  }
+
+  percent(): void {
+    let valuePercent: number;
+    if (this.novoValor != ' ') {
+      switch (this.operacao) {
+        case '+':
+          valuePercent = (parseFloat(this.novoValor) / 100) * parseFloat(this.valorAtual);
+          this.novoValor = valuePercent.toString();
+          break;
+        case '-':
+          valuePercent = (parseFloat(this.novoValor) / 100) * parseFloat(this.valorAtual);
+          this.novoValor = valuePercent.toString();
+          break;
+        case '*':
+          valuePercent = (parseFloat(this.novoValor) / 100);
+          this.novoValor = valuePercent.toString();
+          break;
+        case '/':
+          valuePercent = (parseFloat(this.novoValor) / 100);
+          this.novoValor = valuePercent.toString();
+          break;
+        default:
+          'selecione uma operação correta'
+          break;
+      }
+    }
+  }
 
 }
